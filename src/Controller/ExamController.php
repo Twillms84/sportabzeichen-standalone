57ca50335cba5378e285648fde70ed9e1af883d2
Twillms84 <timo.willms@gmail.com>
2026-02-04 08:59:58 +0100

    Update ExamController.php

--- src/Controller/ExamController.php
+++ src/Controller/ExamController.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/exams', name: 'sportabzeichen_exams_')]
+#[Route('/exams', name: 'app_exams_')]
 final class ExamController extends AbstractController
 {
     #[Route('/', name: 'dashboard')]
