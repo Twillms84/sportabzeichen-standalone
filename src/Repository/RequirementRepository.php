7b4b9ed726a59c3c90619765de4c05bfb8494ade
Twillms84 <timo.willms@gmail.com>
2026-02-04 08:36:19 +0100

    Migration IV

--- src/Repository/RequirementRepository.php
+++ src/Repository/RequirementRepository.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Repository\;
+namespace App\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
