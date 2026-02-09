d904b651633e70e2be7370447a9e8ea454a8e965
Twillms84 <timo.willms@gmail.com>
2026-02-09 23:05:29 +0100

    Update FixAdminCommand.php

--- src/Command/FixAdminCommand.php
+++ src/Command/FixAdminCommand.php
@@ -3,6 +3,7 @@
 namespace App\Command;
 
 use App\Entity\User;
+use App\Entity\Institution; // Wichtig!
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -12,7 +13,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[AsCommand(
     name: 'app:fix-admin',
-    description: 'Setzt das Admin Passwort neu (Sauberer Weg)',
+    description: 'Erstellt Admin und Institution (Schule) neu nach DB-Reset',
 )]
 class FixAdminCommand extends Command
 {
@@ -27,27 +28,70 @@ class FixAdminCommand extends Command
     {
         $email = 'admin@schule.de';
         $password = 'admin123';
+        $schoolName = 'Musterschule';
 
-        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+        // ---------------------------------------------------------
+        // 1. Institution (Schule) sicherstellen
+        // ---------------------------------------------------------
+        $instRepo = $this->entityManager->getRepository(Institution::class);
+        $institution = $instRepo->findOneBy(['name' => $schoolName]);
 
-        if (!$user) {
-            $output->writeln('<error>User ' . $email . ' nicht gefunden!</error>');
-            return Command::FAILURE;
+        if (!$institution) {
+            $output->writeln('<comment>Keine Institution gefunden. Erstelle "' . $schoolName . '"...</comment>');
+            $institution = new Institution();
+            $institution->setName($schoolName);
+            // Falls du ein Feld "key" oder "identifier" in der Institution hast:
+            // $institution->setKey('musterschule'); 
+            
+            $this->entityManager->persist($institution);
+            // Wir flushen hier noch nicht, machen wir am Ende alles zusammen
+        } else {
+            $output->writeln('<info>Institution "' . $schoolName . '" gefunden (ID: ' . $institution->getId() . ').</info>');
         }
 
-        // Das ist der entscheidende Teil: Der offizielle Hasher
+        // ---------------------------------------------------------
+        // 2. User sicherstellen
+        // ---------------------------------------------------------
+        $userRepo = $this->entityManager->getRepository(User::class);
+        $user = $userRepo->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            $output->writeln('<comment>User nicht gefunden. Erstelle neuen Admin...</comment>');
+            $user = new User();
+            $user->setEmail($email);
+            $user->setFirstname('Super'); // Pflichtfelder füllen
+            $user->setLastname('Admin');
+        }
+
+        // ---------------------------------------------------------
+        // 3. Verknüpfung und Passwort
+        // ---------------------------------------------------------
+        
+        // Institution zuweisen (WICHTIG!)
+        $user->setInstitution($institution);
+        
+        // Admin-Rechte geben
+        $user->setRoles(['ROLE_ADMIN']);
+
+        // Passwort hashen
         $hashedPassword = $this->passwordHasher->hashPassword(
             $user,
             $password
         );
-
         $user->setPassword($hashedPassword);
+
+        // ---------------------------------------------------------
+        // 4. Speichern
+        // ---------------------------------------------------------
+        $this->entityManager->persist($user);
         $this->entityManager->flush();
 
-        $output->writeln('<info>ERFOLG!</info>');
-        $output->writeln('User: ' . $email);
-        $output->writeln('Pass: ' . $password);
-        $output->writeln('Hash: ' . $hashedPassword);
+        $output->writeln('----------------------------------------');
+        $output->writeln('<info>ERFOLGREICH WIEDERHERGESTELLT!</info>');
+        $output->writeln('Institution: ' . $institution->getName() . ' (ID: ' . $institution->getId() . ')');
+        $output->writeln('User:        ' . $email);
+        $output->writeln('Passwort:    ' . $password);
+        $output->writeln('----------------------------------------');
 
         return Command::SUCCESS;
     }
