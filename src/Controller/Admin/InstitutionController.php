43d207db110834cf09d7755f6b20dbc913acade6
Twillms84 <timo.willms@gmail.com>
2026-02-17 15:46:08 +0100

    Update InstitutionController.php

--- src/Controller/Admin/InstitutionController.php
+++ src/Controller/Admin/InstitutionController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Repository\InstitutionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
