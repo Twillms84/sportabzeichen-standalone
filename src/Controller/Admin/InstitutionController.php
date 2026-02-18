dfa18d87932837e1e0e6a5fd60a4a577d0495d92
Twillms84 <timo.willms@gmail.com>
2026-02-18 11:17:04 +0100

    Update InstitutionController.php

--- src/Controller/Admin/InstitutionController.php
+++ src/Controller/Admin/InstitutionController.php
@@ -1,17 +1,17 @@
-    <?php
+<?php
 
-    namespace App\Controller\Admin;
+namespace App\Controller\Admin;
 
-    use App\Entity\User;
-    use App\Form\InstitutionType;
-    use App\Repository\InstitutionRepository;
-    use Doctrine\ORM\EntityManagerInterface;
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\HttpFoundation\Request;
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\Annotation\Route;
-    use Symfony\Component\Security\Http\Attribute\IsGranted;
-    use App\Entity\Institution;
+use App\Entity\User;
+use App\Form\InstitutionType;
+use App\Repository\InstitutionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Entity\Institution;
 
     #[Route('/admin/institutions', name: 'admin_institution_')]
     // IsGranted von hier oben entfernen, damit nicht die ganze Klasse gesperrt ist!
