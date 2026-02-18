51c1c4869e048914fb250187f63f8039bcebecff
Twillms84 <timo.willms@gmail.com>
2026-02-18 06:51:02 +0100

    Update InstitutionController.php

--- src/Controller/Admin/InstitutionController.php
+++ src/Controller/Admin/InstitutionController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Entity\Institution;
 
 #[Route('/admin/institutions', name: 'admin_institution_')]
 // IsGranted von hier oben entfernen, damit nicht die ganze Klasse gesperrt ist!
