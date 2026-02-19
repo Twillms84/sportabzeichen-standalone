8b2a4b667917e07b0ed7d1e0a18ef3a3cc5be763
Twillms84 <timo.willms@gmail.com>
2026-02-19 14:03:48 +0100

    Group Managing

--- src/Controller/Admin/GroupController.php
+++ src/Controller/Admin/GroupController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/groups', name: 'app_admin_group_')]
+#[Route('/admin/groups', name: 'admin_group_')]
 class GroupController extends AbstractController
 {
     #[Route('/', name: 'index', methods: ['GET'])]
