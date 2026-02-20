8b80394a2627906181a11011fcf1d5b8b95f6cff
Twillms84 <timo.willms@gmail.com>
2026-02-20 17:04:02 +0100

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route; // WICHTIG: Attribute nutzen!
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -12,14 +12,12 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // Falls der User schon eingeloggt ist, schick ihn zum Dashboard
+        // Wenn schon eingeloggt -> ab ins Admin-Cockpit
         if ($this->getUser()) {
-            return $this->redirectToRoute('app_exams_dashboard');
+            return $this->redirectToRoute('admin_exam_overview');
         }
 
-        // Fehler holen, falls der Login fehlgeschlagen ist
         $error = $authenticationUtils->getLastAuthenticationError();
-        // Letzter Benutzername, den der User eingegeben hat
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
@@ -31,7 +29,6 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        // Diese Methode bleibt leer, Symfony fängt den Request ab
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
